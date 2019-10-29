1d3dafc0e16d302dceeb82c963a10fc7d1c2bdaf
Iarlen Aquiles <iarlensilva775@gmail.com>
2019-10-28 23:01:07 -0300

    animando o arrastar do card para deletar o post

--- components/User.js
+++ components/User.js
@@ -10,16 +10,48 @@ import {
   StyleSheet,
   Dimensions,
   Animated,
-  TouchableWithoutFeedback
+  TouchableWithoutFeedback,
+  PanResponder
 } from "react-native";
 
 import Icon from "react-native-vector-icons/FontAwesome";
 
+const { width } = Dimensions.get("window");
 export default class User extends Component {
   state = {
     opacity: new Animated.Value(0),
     offset: new Animated.ValueXY({ x: 0, y: 50 })
   };
+
+  componentWillMount() {
+    this._panResponder = PanResponder.create({
+      onPanResponderTerminationRequest: () => false,
+      onMoveShouldSetPanResponder: () => true,
+      onPanResponderMove: Animated.event([
+        null,
+        {
+          dx: this.state.offset.x
+        }
+      ]),
+
+      onPanResponderRelease: () => {
+        if (this.state.offset.x._value < -200) {
+          Alert.alert("Deleted!!!");
+        }
+
+        Animated.spring(this.state.offset.x, {
+          toValue: 0,
+          bounciness: 10
+        }).start();
+      },
+      onPanResponderTerminate: () => {
+        Animated.spring(this.state.offset.x, {
+          toValue: 0,
+          bounciness: 10
+        }).start();
+      }
+    });
+  }
 
   componentDidMount() {
     Animated.parallel([
@@ -40,9 +72,18 @@ export default class User extends Component {
 
     return (
       <Animated.View
+        {...this._panResponder.panHandlers}
         style={[
           {
-            transform: [...this.state.offset.getTranslateTransform()]
+            transform: [
+              ...this.state.offset.getTranslateTransform(),
+              {
+                rotateZ: this.state.offset.x.interpolate({
+                  inputRange: [width * -1, width],
+                  outputRange: ["-50deg", "50deg"]
+                })
+              }
+            ]
           },
           { opacity: this.state.opacity }
         ]}
